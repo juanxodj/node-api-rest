5f4a965d91e6e60f52609f49abfc1d025ba02a66
Juan Sánchez <jlsc92@gmail.com>
2023-04-30 23:26:00 -0500

    afip ok

--- src/controllers/afipController.js
+++ src/controllers/afipController.js
@@ -1,61 +1,137 @@
-import Invoice from "@models/invoice";
 import Afip from "@afipsdk/afip.js";
+import fs from "fs";
 
-const index = async (req, res) => {};
+const routeCerts = "./src/assets/certs";
+
+const index = async (req, res) => {
+  return res.status(400).json({ msg: "Index Route" });
+};
+
+const saveCert = (req, res) => {
+  const { cert, key } = req.body;
+
+  fs.writeFile(`${routeCerts}/cert`, cert, (err) => {
+    if (err) throw err;
+    console.log("El cert ha sido creado exitosamente!");
+  });
+
+  fs.writeFile(`${routeCerts}/key`, key, (err) => {
+    if (err) throw err;
+    console.log("El key ha sido creado exitosamente!");
+  });
+
+  return res.status(200).json({ msg: "Certs Updated" });
+};
+
+const salesPoint = async (req, res) => {
+  const { cuit, production } = req.query;
+
+  try {
+    const points = await getPoints(cuit, production);
+    return res.status(200).json(points);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const lastVoucher = async (req, res) => {
+  const { cuit, production, salesPoint, invoiceType } = req.query;
+
+  const afip = new Afip({
+    CUIT: cuit,
+    production: production === "true" ? true : false,
+    res_folder: routeCerts,
+  });
+
+  try {
+    const lastVoucher = await afip.ElectronicBilling.getLastVoucher(
+      salesPoint,
+      invoiceType
+    );
+    return res.status(200).json(lastVoucher);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
 
 const store = async (req, res) => {
   const {
-    CantReg, // Cantidad de comprobantes a registrar
-    PtoVta, // Punto de venta
-    CbteTipo, // Tipo de comprobante (ver tipos disponibles)
-    Concepto, // Concepto del Comprobante: (1)Productos, (2)Servicios, (3)Productos y Servicios
-    DocTipo, // Tipo de documento del comprador (99 consumidor final, ver tipos disponibles)
-    DocNro, // Número de documento del comprador (0 consumidor final)
-    CbteDesde, // Número de comprobante o numero del primer comprobante en caso de ser mas de uno
-    CbteHasta, // Número de comprobante o numero del último comprobante en caso de ser mas de uno
-    ImpTotal, // Importe total del comprobante
-    ImpTotConc, // Importe neto no gravado
-    ImpNeto, // Importe neto gravado
-    ImpOpEx, // Importe exento de IVA
-    ImpIVA, //Importe total de IVA
-    ImpTrib, //Importe total de tributos
-    MonId, //Tipo de moneda usada en el comprobante (ver tipos disponibles)('PES' para pesos argentinos)
-    MonCotiz, // Cotización de la moneda usada (1 para pesos argentinos)
-    Iva, // (Opcional) Alícuotas asociadas al comprobante
+    ptoVta,
+    cbteTipo, // Tipo de comprobante (ver tipos disponibles)
+    concepto, // concepto del Comprobante: (1)Productos, (2)Servicios, (3)Productos y Servicios
+    docTipo, // Tipo de documento del comprador (99 consumidor final, ver tipos disponibles)
+    docNro, // Número de documento del comprador (0 consumidor final)
+    impTotal, // Importe total del comprobante
+    impTotConc, // Importe neto no gravado
+    impNeto, // Importe neto gravado
+    impOpEx, // Importe exento de IVA
+    impIVA, //Importe total de IVA
+    impTrib, //Importe total de tributos
+    iva, // Alícuotas asociadas al comprobante
   } = req.body;
+
+  const { cuit, production } = req.query;
 
   const dateNow = new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
     .toISOString()
     .split("T")[0];
-  const { body: invoiceData } = req;
-  invoiceData.CbteFch = parseInt(dateNow.replace(/-/g, ""));
 
-  try {
-    const invoice = await Invoice.create(invoiceData);
-    return res.status(200).json(invoice);
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
+  const invoiceDataDefault = {
+    CantReg: 1,
+    CbteFch: parseInt(dateNow.replace(/-/g, "")),
+    MonId: "PES",
+    MonCotiz: 1,
+  };
 
   const afip = new Afip({
-    CUIT: 20111111112, // CUIT del contribuyente
-    production: false, // (Opcional) true para usar los Web Services en modo producción
-    cert: "certificado.crt", // (Opcional) ruta donde se encuentra el certificado, relativa a res_folder
-    key: "clave.key", // (Opcional) ruta donde se encuentra el certificado, relativa a res_folder
-    res_folder: "./certs", // (Opcional) Ruta de la carpeta donde se encuentran el cert y key
-    ta_folder: "clave_del_certificado", // (Opcional) Ruta de la carpeta donde se guardaran los Token
+    CUIT: cuit,
+    production: production === "true" ? true : false,
+    res_folder: routeCerts,
   });
 
   try {
-    const data = await afip.ElectronicBilling.createVoucher(invoiceData, true);
+    const lastVoucher = await afip.ElectronicBilling.getLastVoucher(
+      ptoVta,
+      cbteTipo
+    );
 
-    console.log(data["CAE"]); //CAE asignado el comprobante
-    console.log(data["CAEFchVto"]); //Fecha de vencimiento del CAE (yyyy-mm-dd)
+    invoiceDataDefault.CbteDesde = lastVoucher + 1;
+    invoiceDataDefault.CbteHasta = lastVoucher + 1;
 
+    const data = await afip.ElectronicBilling.createVoucher(
+      {
+        ...invoiceDataDefault,
+        PtoVta: ptoVta,
+        CbteTipo: cbteTipo,
+        Concepto: concepto,
+        DocTipo: docTipo,
+        DocNro: docNro,
+        ImpTotal: impTotal,
+        ImpTotConc: impTotConc,
+        ImpNeto: impNeto,
+        ImpOpEx: impOpEx,
+        ImpIVA: impIVA,
+        ImpTrib: impTrib,
+        Iva: iva,
+      },
+      true
+    );
+    console.log("Invoice Created");
+    console.log(data);
     return res.status(200).json(data);
   } catch (error) {
-    console.log(error);
+    return res.status(400).json({ error: error.message });
   }
 };
 
-export { index, store };
+async function getPoints(cuit, production) {
+  const afip = new Afip({
+    CUIT: cuit,
+    production: production === "true" ? true : false,
+    res_folder: routeCerts,
+  });
+
+  return await afip.ElectronicBilling.getSalesPoints();
+}
+
+export { index, store, salesPoint, saveCert, lastVoucher };
